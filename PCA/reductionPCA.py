48a17fc1388e8c929517aaee663bde3e4e6f7c7e
oierajenjo <oierajenjo@opendeusto.es>
2021-04-21 16:30:18 +0200

    PCA working

--- PCA/reductionPCA.py
+++ PCA/reductionPCA.py
@@ -6,50 +6,51 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-import Resources.retrieveData as retrieveData
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
 path_parent = os.path.dirname(os.getcwd())
 os.chdir(path_parent)
-print(os.getcwd())
+# print(os.getcwd())
+file = os.getcwd() + '\Resources\\allData.csv'
 
-if not path.exists(os.getcwd()+'\Resources\\allData.csv'):
-    print(1)
-    os.system(os.getcwd()+'\Resources\\retrieveData.py')
-    print(2)
+if not path.exists(file):
+    os.system(os.getcwd() + '\Resources\\retrieveData.py')
 
-
-# # Importing Dataset
+# Importing Dataset
 # names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
-# dataset = pd.read_csv(url, names=names)
-# dataset.head()
-# # print("DATASET")
-# # print(dataset)
-#
-# # Preprocessing
-# X = dataset.drop('Class', 1)
-# y = dataset['Class']
-#
+dataset = pd.read_csv(file)
+dataset.head()
+# print("DATASET")
+# print(dataset)
+
+# Preprocessing
+X = dataset.drop(['Test_nr', 'Faulty'], 1)
+Y = dataset[['Test_nr', 'Faulty']]
+# print("X")
+# print(X)
+# print("Y")
+# print(Y)
+
 # # Splitting the dataset into the Training set and Test set
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-#
-# # STANDARDISE
-# sc = StandardScaler()
-# X_train = sc.fit_transform(X_train)
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5)
+
+# STANDARDISE
+sc = StandardScaler()
+X = sc.fit_transform(X)
 # X_test = sc.transform(X_test)
-#
-# # Applying PCA
-# pca = PCA()
-# X_train = pca.fit_transform(X_train)
+
+# Applying PCA
+pca = PCA(n_components=3)
+X = pca.fit_transform(X)
 # X_test = pca.transform(X_test)
-#
-# explained_variance = pca.explained_variance_ratio_
-#
-# # # 1 principal component to train our algorithm
-# # pca = PCA(n_components=1)
-# # X_train = pca.fit_transform(X_train)
-# # X_test = pca.transform(X_test)
-#
-#
-# print(X_train)
+
+explained_variance = pca.explained_variance_ratio_
+
+print(X)
 # print(X_test)
-# print(explained_variance)
+
+print("Explained Variance")
+print(explained_variance)
+
