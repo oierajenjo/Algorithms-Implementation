f728799ae34d56afb66a069f02d6f14ac60acf2c
oierajenjo <oierajenjo@opendeusto.es>
2021-05-06 12:59:19 +0200

    PCA finished

--- PCA/reductionPCA.py
+++ PCA/reductionPCA.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 
-def reductionPCA(dataset, conditions):
+def reductionPCA(dataset, accuracy):
     # Preprocessing
     dataset = dataset.loc[(dataset['HeatLoad'] == 10000) & ((dataset['T_set'] == 0) | (dataset['T_set'] == 12))
                           & ((dataset['Cpr_Scale'] >= 0.9) | (dataset['Cpr_Scale'] == 0.3))]
@@ -23,50 +23,85 @@ def reductionPCA(dataset, conditions):
     print("Y")
     print(Y.head())
 
+    # Set Test and training data
+    X_tr, X_te, Y_train, Y_test = train_test_split(X, Y, test_size=0.01, random_state=0)
+
     # STANDARDISE
     sc = StandardScaler()
-    X = sc.fit_transform(X)
+    X_train = sc.fit_transform(X_tr)
+    X_test = sc.transform(X_te)
 
+    # Amount of PCs to keep
     # Applying PCA
     pca = PCA()
-    PC_X = pca.fit_transform(X)
+    pca.fit_transform(X_train)
     explained_variance = pca.explained_variance_ratio_
+    print("Explained Variance")
+    print(explained_variance)
+
+    # Explained Variance Ratio PLOT
+    fig, ax = plt.subplots()
     columns = ['PC' + str(i) for i in range(1, len(explained_variance) + 1)]
-    PC_X = pd.DataFrame(data=PC_X, columns=columns)
-    finalX = pd.concat([PC_X, Y], axis=1)
-    print("Final X")
-    print(finalX.loc[finalX['Faulty'] == 0].tail())
+    ax.bar(columns, explained_variance)
+    plt.title("Explained Variance per PC")
+    plt.ylabel("Explained Variance Ratio")
+    plt.xlabel("Principal Components")
+    plt.show()
+    fig.savefig(os.getcwd() + '/results/PC.png')
+
+    val = 0
+    n = 0
+    for ev in explained_variance:
+        val += ev
+        n += 1
+        if val >= accuracy:
+            break
+    print(val)
+    print(n)
+
+    # Applying PCA
+    pca = PCA(n_components=n)
+    pc_x_train = pca.fit_transform(X_train)
+    pc_x_test = pca.transform(X_test)
+    ex_var = pca.explained_variance_ratio_
+
+    # PC DataFrame
+    columns = ['PC' + str(i) for i in range(1, len(ex_var) + 1)]
+    pc_x_train = pd.DataFrame(data=pc_x_train, columns=columns)
+    pca_train = pd.concat([pc_x_train, Y_train.reset_index()], axis=1)
+
+    pc_x_test = pd.DataFrame(data=pc_x_test, columns=columns)
+    pca_test = pd.concat([pc_x_test, Y_test.reset_index()], axis=1)
+
+    print("Final PCA X")
+    print(pca_train)
 
     # PLOTTING
+    fig, ax = plotting(pca_train)
+    ax.set_title('3 component PCA', fontsize=20)
+    fig.show()
+    fig.savefig('results/3d.png')
+
+    return pc_x_train, Y_train.reset_index(), pc_x_test, Y_test.reset_index(), explained_variance, pca_train, pca_test
+
+
+def plotting(pca_train):
     fig = plt.figure(figsize=(8, 8))
     ax = plt.axes(projection='3d')
     # ax = fig.add_subplot(1, 1, 1)
     ax.set_xlabel('Principal Component 1', fontsize=15)
     ax.set_ylabel('Principal Component 2', fontsize=15)
     ax.set_zlabel('Principal Component 3', fontsize=15)
-    ax.set_title('3 component PCA', fontsize=20)
     targets = [0, 1]
     colors = ['g', 'r']
-    for target, color in zip(targets, colors):
-        indicesToKeep = finalX['Faulty'] == target
-        ax.scatter3D(finalX.loc[indicesToKeep, 'PC1'],
-                     finalX.loc[indicesToKeep, 'PC2'],
-                     finalX.loc[indicesToKeep, 'PC3'],
-                     c=color)
-
+    markers = ['*', '*']
+    for target, color, m in zip(targets, colors, markers):
+        indicesToKeep = pca_train['Faulty'] == target
+        ax.scatter3D(pca_train.loc[indicesToKeep, 'PC1'],
+                     pca_train.loc[indicesToKeep, 'PC2'],
+                     pca_train.loc[indicesToKeep, 'PC3'],
+                     c=color,
+                     marker=m)
     ax.legend(['Not Faulty', 'Faulty'])
     ax.grid()
-    fig.show()
-    fig.savefig('results/3d.png')
-
-    print(PC_X)
-    print("Explained Variance")
-    print(explained_variance)
-
-    fig, ax = plt.subplots()
-    ax.bar(columns, explained_variance)
-    plt.title("Principal Components")
-    plt.show()
-    fig.savefig(os.getcwd() + '/results/PC.png')
-
-    return PC_X, Y, explained_variance, finalX
+    return fig, ax
