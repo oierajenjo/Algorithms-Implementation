a89b66b09f680e1e548bd50de69c60d5b7aa1847
oierajenjo <oierajenjo@opendeusto.es>
2021-05-17 17:30:39 +0200

    All working

--- Resources/functions.py
+++ Resources/functions.py
@@ -3,35 +3,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def set_train_validation_testData(pc_x, Y, amount_pcs):
-    columns = ['PC' + str(i) for i in range(1, amount_pcs + 1)]
-    X = pd.DataFrame(data=pc_x, columns=columns)
-
-    # Set Test and training data
-    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.01, random_state=42)
-    X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.01, random_state=42)
-
-    # Remove previous index
-    X_train = reset_ind(X_train)
-    Y_train = reset_ind(Y_train)
-    X_val = reset_ind(X_val)
-    Y_val = reset_ind(Y_val)
-    X_test = reset_ind(X_test)
-    Y_test = reset_ind(Y_test)
-
-    save_csv_file('/data/trainData.csv', pd.concat([X_train, Y_train], axis=1))
-    save_csv_file('/data/validationData.csv', pd.concat([X_val, Y_val], axis=1))
-    save_csv_file('/data/testData.csv', pd.concat([X_test, Y_test], axis=1))
-
-    return X_train, Y_train, X_val, Y_val, X_test, Y_test
-
-
 def get_df(file):
-    df = pd.read_csv(file)
-    df.head()
-    df = df.loc[(df['HeatLoad'] == 10000) & ((df['T_set'] == 0) | (df['T_set'] == 12))
-                & ((df['Cpr_Scale'] >= 0.9) | (df['Cpr_Scale'] == 0.3))]
-    return df
+    d = pd.read_csv(file)
+    d.head()
+    d = d.loc[(d['HeatLoad'] == 10000) & ((d['T_set'] == 0) | (d['T_set'] == 12))
+              & ((d['Cpr_Scale'] >= 0.9) | (d['Cpr_Scale'] == 0.3))]
+    d = reset_ind(d)
+    return d
 
 
 def preprocess_data(df):
@@ -44,6 +22,25 @@ def preprocess_data(df):
     # print("Y")
     # print(Y.head())
     return X, Y
+
+
+def set_train_validation_testData(pc_X, Y_all, amount_pcs):
+    columns = ['PC' + str(i) for i in range(1, amount_pcs + 1)]
+    X_all = pd.DataFrame(data=pc_X, columns=columns)
+
+    # Set Test and training data
+    X_train, X_val, Y_train, Y_val = train_test_split(X_all, Y_all, test_size=0.01, random_state=42)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.01, random_state=42)
+
+    # Remove previous index
+    X_train = reset_ind(X_train)
+    Y_train = reset_ind(Y_train)
+    X_val = reset_ind(X_val)
+    Y_val = reset_ind(Y_val)
+    X_test = reset_ind(X_test)
+    Y_test = reset_ind(Y_test)
+
+    return X_all, X_train, Y_train, X_val, Y_val, X_test, Y_test
 
 
 def read_csv_data(file, conditions=None):
--- main.py
+++ main.py
@@ -2,129 +2,142 @@ import os
 from datetime import datetime
 from time import process_time
 
-
+import pandas as pd
 from matplotlib import pyplot as plt
 
 from PCA.reductionPCA import reductionPCA, plotting, standardise
 from RBFN.classifierRBFN import train_data, measure_accuracy, get_XY, amount_centroids, plot_centroids
-from Resources.functions import get_df, preprocess_data, read_csv_data, set_train_validation_testData, pca_df
+from Resources.functions import get_df, preprocess_data, read_csv_data, set_train_validation_testData, pca_df, \
+    save_csv_file
 from Resources.retrieveData import download_file_from_google_drive
 
+accuracy = 0.995  # Accuracy for the PCA
 
-def main():
-    file_all = os.getcwd() + '/data/allData.csv'
-    file_noisy = os.getcwd() + '/data/allNoisyData.csv'
-    file_train = os.getcwd() + '/data/trainData.csv'
-    file_validation = os.getcwd() + '/data/validationData.csv'
-    file_test = os.getcwd() + '/data/testData.csv'
+root = os.getcwd()
+file_all = '/data/allData.csv'
+file_noisy = '/data/allNoisyData.csv'
+file_merged = '/data/mergedData(' + str(accuracy) + ').csv'
+file_train = '/data/trainData(' + str(accuracy) + ').csv'
+file_val = '/data/validationData(' + str(accuracy) + ').csv'
+file_test = '/data/testData(' + str(accuracy) + ').csv'
+
+"""
+CHECK IF FILES EXIST
+If not execute download_file_from_google_drive
+"""
+if not os.path.exists(root + file_all):
+    download_file_from_google_drive('allData.csv')
+
+if not os.path.exists(root + file_noisy):
+    download_file_from_google_drive('allNoisyData.csv')
+
+if not os.path.exists(root + file_val) or not os.path.exists(root + file_train) \
+        or not os.path.exists(root + file_test):
+    # Importing Datasets and group them
+    df = get_df(root + file_all)
+    print("Number of non-noisy samples: " + str(len(df.index)))
+    dfn = get_df(root + file_noisy)
+    print("Number of noisy samples: " + str(len(dfn.index)))
+    df_all = pd.concat([df, dfn], ignore_index=True)
+
+    # Preprocessing
+    X, Y = preprocess_data(df_all)
 
     """
-    CHECK IF FILES EXIST
-    If not execute download_file_from_google_drive
+    STANDARDISATION
+    Standardisation process were data is standardised to mean 0 and var 1
     """
-    if not os.path.exists(file_all):
-        download_file_from_google_drive('allData.csv')
+    s_X = standardise(X)
 
-    if not os.path.exists(file_noisy):
-        download_file_from_google_drive('allNoisyData.csv')
+    """
+    PCA
+    If don't want to implement PCA comment this part
+    """
+    pc_X, explained_variance, amount_pcs = reductionPCA(s_X, accuracy)
 
     """
     SEPARATE DATA
     Data samples are separated in train, validation and test data
     """
+    X_all, X_train, Y_train, X_val, Y_val, X_test, Y_test = set_train_validation_testData(pc_X, Y, amount_pcs)
 
-    if not os.path.exists(file_validation) or not os.path.exists(file_train) or not os.path.exists(file_test):
-        # Importing Dataset
-        df = get_df(file_all)
-        dfn = get_df(file_noisy)
-        df.append(dfn)
+    save_csv_file(file_train, pd.concat([X_train, Y_train], axis=1))
+    save_csv_file(file_val, pd.concat([X_val, Y_val], axis=1))
+    save_csv_file(file_test, pd.concat([X_test, Y_test], axis=1))
+    save_csv_file(file_merged, pd.concat([X_all, Y], axis=1))
+else:
+    X_all, Y = read_csv_data(root + file_merged)
+    X_train, Y_train = read_csv_data(root + file_train)
+    X_val, Y_val = read_csv_data(root + file_val)
+    X_test, Y_test = read_csv_data(root + file_test)
 
-        # Preprocessing
-        X, Y = preprocess_data(df)
-        """
-        STANDARDISATION
-        Standardisation process were data is standardised to mean 0 and var 1
-        """
-        X = standardise(X)
+amount_pcs = len(X_train.columns)
+print(amount_pcs)
 
-        """
-        PCA
-        If don't want to implement PCA comment this part
-        """
-        accuracy = 0.9995
-        pc_X, explained_variance, amount_pcs = reductionPCA(X, accuracy)
+"""
+PCA Matrix
+Generate 'PCA' matrix
+"""
+pca_all = pca_df(X_all, Y)
+pca_train = pca_df(X_train, Y_train)
+pca_validation = pca_df(X_val, Y_val)
+pca_test = pca_df(X_test, Y_test)
+print("Training  PCA")
+print(pca_train)
 
-        X_train, Y_train, X_validation, Y_validation, X_test, Y_test = set_train_validation_testData(pc_X, Y
-                                                                                                     , amount_pcs)
+# Plotting PCA training data
+fig, ax = plotting(pca_all)
+ax.set_title('3 component PCA All', fontsize=20)
+ax.legend(['Non Faulty', 'Faulty'])
+fig.show()
+fig.savefig('results/3d(' + datetime.today().strftime("%d%m%Y_%H-%M.%S") + ').png')
 
-    else:
-        X_train, Y_train = read_csv_data(file_train)
-        X_validation, Y_validation = read_csv_data(file_validation)
-        X_test, Y_test = read_csv_data(file_test)
+"""
+RBFN
+Instead of validation use test data
+If don't want to implement RBFN comment this part
+"""
+# Accuracy per amount of centroid
+centroids_max = 100
+step = 1
+init_centroid = 2
+scores, c_all = amount_centroids(pca_train, pca_validation, amount_pcs, centroids_max, step=step
+                                 , init_centroid=init_centroid)
 
-    amount_pcs = len(X_train.columns)
-    print(amount_pcs)
+# Plotting accuracy per Amount of Centroids
+plt.plot(c_all, scores)
+plt.title('Accuracy per Amount of Centroids')
+plt.xlabel('Amount of Centroids')
+plt.ylabel('Accuracy')
+plt.grid()
+plt.show()
+fig.savefig('results/Accuracy-Centroids(' + str(c_all.head(1).values) + '-' + str(c_all.tail(1).values) + ').png')
 
-    """
-    PCA Matrix
-    Generate 'PCA' matrix
-    """
-    pca_train = pca_df(X_train, Y_train)
-    pca_validation = pca_df(X_validation, Y_validation)
-    pca_test = pca_df(X_test, Y_test)
-    print("Final PCA X")
-    print(pca_train)
+"""
+RBFN
+Accuracy with noisy data
+"""
+dfs = pd.read_csv(root + '/results/score_cent(2-100).csv')
+dfs.head()
+c_all = dfs['centroids'].tolist()
+scores = dfs['scores'].tolist()
+X_pca_train, Y_pca_train = get_XY(pca_train, amount_pcs)
+n_cent = c_all[scores.index(max(scores))]
+centroids, W, sigma = train_data(X_pca_train, Y_pca_train, n_centroids=n_cent)
 
-    # Plotting PCA training data
-    fig, ax = plotting(pca_train)
-    ax.set_title('3 component PCA Train', fontsize=20)
-    ax.legend(['Non Faulty', 'Faulty'])
-    fig.show()
-    fig.savefig('results/3d(' + datetime.today().strftime("%d%m%Y_%H-%M.%S") + ').png')
+# Plotting centroids
+plot_centroids(pca_train, centroids, 'results/' + str(n_cent) + 'CentroidsTrain-Optimal.png')
 
-    """
-    RBFN
-    Instead of validation use test data
-    If don't want to implement RBFN comment this part
-    """
-    # Accuracy per amount of centroid
-    # centroids_max = int(len(pca_train.index)/10)
-    centroids_max = 100
-    step = 1
-    scores, c_all = amount_centroids(pca_train, pca_validation, amount_pcs, centroids_max, step)
+t1_start = process_time()
+X_pca_test, Y_pca_test = get_XY(pca_test, amount_pcs)
+score = measure_accuracy(X_pca_test, Y_pca_test, centroids, sigma, W)
+t1_stop = process_time()
+print("Testing time in seconds:", t1_stop - t1_start)
+print("Score: " + str(score) + "%")
 
-    # Plotting accuracy per Amount of Centroids
-    plt.plot(c_all, scores)
-    plt.title('Accuracy per Amount of Centroids')
-    plt.xlabel('Amount of Centroids')
-    plt.ylabel('Accuracy')
-    plt.grid()
-    plt.show()
-    fig.savefig('results/Accuracy-Centroids(' + str(c_all[0]) + '-' + str(c_all[-1]) + ').png')
-
-    """
-    RBFN
-    Accuracy with noisy data
-    """
-    X_pca_train, Y_pca_train = get_XY(pca_train, amount_pcs)
-    centroids, W, sigma = train_data(X_pca_train, Y_pca_train, n_centroids=centroids_max)
-
-    # Plotting centroids
-    plot_centroids(pca_train, centroids)
-
-    t1_start = process_time()
-    X_pca_test, Y_pca_test = get_XY(pca_test, amount_pcs)
-    score = measure_accuracy(X_pca_test, Y_pca_test, centroids, sigma, W)
-    t1_stop = process_time()
-    print("Testing time in seconds:", t1_stop - t1_start)
-
-    fig, ax = plotting(pca_test)
-    ax.set_title('3 component PCA Test', fontsize=20)
-    ax.legend(['Non Faulty', 'Faulty'])
-    fig.show()
-    fig.savefig('results/3d(' + datetime.today().strftime("%d%m%Y_%H-%M.%S") + ').png')
-    plot_centroids(pca_test, centroids)
-
-
-if __name__ == "__main__":
-    main()
+fig, ax = plotting(pca_test)
+ax.set_title('3 component PCA Test', fontsize=20)
+ax.legend(['Non Faulty', 'Faulty'])
+fig.show()
+fig.savefig('results/3d(' + datetime.today().strftime("%d%m%Y_%H-%M.%S") + ').png')
+plot_centroids(pca_test, centroids, 'results/' + str(n_cent) + 'CentroidsTest-Optimal.png')
